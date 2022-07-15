20c5b3895c41305ed6aca7e2118f30b82f6fb5c9
solanki <anujaj2222.gmail.com>
2022-07-15 22:26:32 +0530

    Adding Five  Random Dice Number And Calculating There Avarage

---
+++ sequanceProblem/pro4.js
@@ -0,0 +1,10 @@
+let digit1=Math.floor(Math.random()*100);
+let digit2=Math.floor(Math.random()*100);
+let digit3=Math.floor(Math.random()*100);
+let digit4=Math.floor(Math.random()*100);
+let digit5=Math.floor(Math.random()*100);
+
+
+let result = digit1+ digit2 + digit3+ digit4+ digit5;
+let avg=result/5;
+console.log("Sum Of Numbers Is " +result+"\n"+"Avrage OF Numbers Is  "+avg);
