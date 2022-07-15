68bbd2cbe353e0e295b6264fb81010ae658f446a
solanki <anujaj2222.gmail.com>
2022-07-15 23:07:35 +0530

    Doing Unit Corversion

---
+++ sequanceProblem/pro5.js
@@ -0,0 +1,21 @@
+//a.
+
+let inch=1/12;
+let result= 42*inch;
+
+console.log(result+" ft");
+
+//b.
+
+let feet=0.3048;
+let areaInMeter=(60*feet)*(40*feet);
+
+console.log("Area Of One Plot In Meter Squre is => "+areaInMeter+" m^2")
+
+//c.
+
+let OneacresInMeterSqure =4047;
+let plotAreaInMeterSqure=areaInMeter*25;
+let plotAreaInAcer=plotAreaInMeterSqure/OneacresInMeterSqure;
+
+console.log("Area Of 25 Plot In Acers Is => "+plotAreaInAcer+" Acre");
