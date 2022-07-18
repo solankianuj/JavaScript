d028f9ffac9ae17d7d328868025dba28eb516f1d
solanki <anujaj2222.gmail.com>
2022-07-18 21:35:24 +0530

    Selections Problem

--- SelectionProblem/pro3.js
+++ SelectionProblem/pro3.js
@@ -1,19 +1,19 @@
-let year=1800;
+let year=1600;
 
 if( year%4==0 ){
 
-    if(year % 400 == 0){
+    if(year % 100 == 0){
 
-        if(year % 100 != 0){
+        if(year % 400 == 0){
             console.log("year is a leap year");
         
         }
         else{
-           console.log("year is a leap year");
+           console.log("year is a not leap year");
         }
     }
      else{
-        console.log("year is not a leap year");
+        console.log("year is a leap year");
     }
 }
 
