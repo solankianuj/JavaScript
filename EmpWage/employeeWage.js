1d7c1b19f590ffba9ed39728e2000c63c1392edb
solanki <anujaj2222.gmail.com>
2022-07-25 22:14:12 +0530

    Adding_Function and Calcilating EmpWage

--- EmpWage/employeeWage.js
+++ EmpWage/employeeWage.js
@@ -7,15 +7,18 @@ const wage_per_Hr=20;
 let empHr=0;
 let empcheck=Math.floor(Math.random()*10)%3;
 
-switch (empcheck) {
-    case IS_Full_Time: empHr=full_time_hr;
-        
-        break;
-    case IS_Part_Time: empHr=part_Time_Hr;
+function getWorkingHr(empcheck) {
+    switch (empcheck) {
+        case IS_Full_Time: return full_time_hr;
+            
             break;
-    default: empHr=0;
-        break;
+        case IS_Part_Time: return part_Time_Hr;
+                break;
+        default: return 0;
+            break;
+    }
+    
 }
-
+empHr=getWorkingHr(empcheck);
 let empWage=empHr*wage_per_Hr;
 console.log("Employe Daily Wage IS : "+empWage);
