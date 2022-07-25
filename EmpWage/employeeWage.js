e7128cd11c9e1dff8a4ed77d668932b2efbd08f6
solanki <anujaj2222.gmail.com>
2022-07-25 23:20:45 +0530

    Calculating_Wage_Till_Max_Condition_Of_Month

--- EmpWage/employeeWage.js
+++ EmpWage/employeeWage.js
@@ -3,7 +3,8 @@ const IS_Full_Time=1;
 const part_Time_Hr=4;
 const full_time_hr=8;
 const wage_per_Hr=20;
-const num_Of_Working=2;
+const max_Working_days=20;
+const max_working_Hr=160;
 
 let empHr=0;
 function getWorkingHr(empcheck) {
@@ -18,12 +19,13 @@ function getWorkingHr(empcheck) {
     }
     
 }
-
-for (let i = 0; i < num_Of_Working; i++) {
+total_empDay=0;
+total_empHr=0;
+do {
+    total_empDay++;
     let empcheck=Math.floor(Math.random()*10)%3;
-    empHr +=getWorkingHr(empcheck);
+    total_empHr +=getWorkingHr(empcheck);
     
-}
-
-let empWage=empHr*wage_per_Hr;
-console.log("Employe Daily Wage IS : "+empWage);
+} while (total_empHr<=max_working_Hr && total_empDay<max_Working_days);
+let empWage=total_empHr*wage_per_Hr;
+console.log("Total Days: "+total_empDay+" TotalHr: "+total_empHr+" Employe Daily Wage IS : "+empWage);
--- loopProblem/forloop.js
+++ loopProblem/forloop.js
@@ -1,8 +1,13 @@
 let dogs=["bulldog","pitbull","labrador"];
-
+i=0;
 alldogs="";
-for (let dog of dogs) {
-    alldogs += dog + " ";
-    console.log(alldogs);
-}
+// while (i<dogs.length) {
+//     alldogs += dogs[i++] + " ";
+//     console.log(alldogs);
+// }
+
+
+do{
+alldogs+= dogs[i++]+" ";
+}while(i<dogs.length)
 console.log("New :" +alldogs);
